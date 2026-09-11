88a5491ce41f9f4a9d1bd481075608d298357c17
Enno Yeks <ennoyeks@gmail.com>
2026-09-11 12:45:33 +0300

    Restore Studio via CDN loader from last good commit (interim)

--- js/hshs-upload.js
+++ js/hshs-upload.js
@@ -1,1 +1,8 @@
-SEE_NEXT
+/* temporary: load last known-good Studio from commit until full file is restored */
+(function () {
+  if (window.__hshsUpload) return;
+  var s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/EnnoYeks/SCHOOL-GALLERY@255cb6608129cf952fb2c8de957a7254ae830099/js/hshs-upload.js';
+  s.async = false;
+  document.head.appendChild(s);
+})();
