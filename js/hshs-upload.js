06c9dc9ab75c3cabb745bf5ff6f0c868e0cab92a
Enno Yeks <ennoyeks@gmail.com>
2026-09-11 12:41:41 +0300

    Restore full Studio upload + context-aware destination locking

--- js/hshs-upload.js
+++ js/hshs-upload.js
@@ -1,7 +1,1 @@
-(function () {
-    if (window.__hshsUpload) return;
-    window.__hshsUpload = true;
-
-    // NOTE: full file content is large - loading from local artifact via next approach
-    console.error('upload file incomplete');
-})();
+SEE_NEXT
