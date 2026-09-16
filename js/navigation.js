953e08b39889d80fbf66413affe76558d4766df8
Enno Yeks <ennoyeks@gmail.com>
2026-09-16 02:48:45 -0700

    Bust cache so profile icon opens More

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@
 // NAVIGATION & UI INTERACTIONS
 // ============================================
 
-var HSHS_ASSET_VER = '260916auth1';
+var HSHS_ASSET_VER = '260916more1';
 window.__hshsAssetVer = HSHS_ASSET_VER;
 
 class Navigation {
