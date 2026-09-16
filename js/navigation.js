f5bb1096684a3ddded4361fa4c874320fbd442e0
Enno Yeks <ennoyeks@gmail.com>
2026-09-16 02:24:05 -0700

    Bust asset cache after desktop flex width change

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@
 // NAVIGATION & UI INTERACTIONS
 // ============================================
 
-var HSHS_ASSET_VER = '260916desk1';
+var HSHS_ASSET_VER = '260916desk2';
 window.__hshsAssetVer = HSHS_ASSET_VER;
 
 class Navigation {
