dc9b9fbcfc2eef73dc1506e054706c356d6cef63
Enno Yeks <ennoyeks@gmail.com>
2026-09-21 14:09:06 +0300

    Bump assets to 260921people2 so people layer cache-busts.

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@
 // NAVIGATION & UI INTERACTIONS
 // ============================================
 
-var HSHS_ASSET_VER = '260919real1';
+var HSHS_ASSET_VER = '260921people2';
 window.__hshsAssetVer = HSHS_ASSET_VER;
 
 class Navigation {
