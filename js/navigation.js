12588f035394ca9250b3d7620c52087c9cabb9ee
Enno Yeks <ennoyeks@gmail.com>
2026-09-24 12:32:25 +0300

    Bump asset version to 260924fix2

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@
 // NAVIGATION & UI INTERACTIONS
 // ============================================
 
-var HSHS_ASSET_VER = '260924look1';
+var HSHS_ASSET_VER = '260924fix2';
 window.__hshsAssetVer = HSHS_ASSET_VER;
 
 class Navigation {
